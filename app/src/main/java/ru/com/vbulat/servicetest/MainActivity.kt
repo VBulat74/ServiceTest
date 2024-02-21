dd3687b272149f7263a9356903e451b8ffe76080
VB <Vladimir.Bulat@gmail.com>
2024-02-21 14:40:39 +0300

    Lesson_03 (Working with notifications. NotificationManager)

--- app/src/main/java/ru/com/vbulat/servicetest/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/servicetest/MainActivity.kt
@@ -1,7 +1,12 @@
 package ru.com.vbulat.servicetest
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import ru.com.vbulat.servicetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var id = 0
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -17,5 +24,36 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    @SuppressLint("NotificationPermission")
+    private fun showNotification() {
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHSNEL_ID,
+                CHAMEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHSNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+
+        notificationManager.notify(++ id, notification)
+    }
+
+    companion object {
+        private const val CHSNEL_ID = "chanel_id"
+        private const val CHAMEL_NAME = "chanel_name"
     }
 }
