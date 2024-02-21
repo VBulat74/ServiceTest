7749e82f4d95ab67437b9b0cb9f7670be32d43d0
VB <Vladimir.Bulat@gmail.com>
2024-02-21 10:59:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/com/vbulat/servicetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.com.vbulat.servicetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
