55568a56d3e5e5ed3878d682a97b691363bfaad2
VB <Vladimir.Bulat@gmail.com>
2024-02-21 11:07:09 +0300

    Lesson_01 (preparation)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    //###coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/ru/com/vbulat/servicetest/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/servicetest/MainActivity.kt
@@ -2,8 +2,14 @@ package ru.com.vbulat.servicetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.com.vbulat.servicetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
